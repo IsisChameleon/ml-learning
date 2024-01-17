4d1225ddd450285deed08d685ff5eac751a409c4
IsisChameleon <12711084+IsisChameleon@users.noreply.github.com>
2024-01-17 05:48:33 +0000

    Library before unit tests

--- src/phages/modules/library.py
+++ src/phages/modules/library.py
@@ -8,9 +8,15 @@ from llama_index import SimpleDirectoryReader
 from pathlib import Path
 from llama_index.embeddings import OpenAIEmbedding
 from llama_index.text_splitter import SentenceSplitter
-from llama_index.extractors import TitleExtractor
-from llama_index.ingestion import IngestionPipeline, IngestionCache
 from llama_index import SimpleDirectoryReader
+from llama_index.extractors import (
+    SummaryExtractor,
+    QuestionsAnsweredExtractor,
+    TitleExtractor,
+    KeywordExtractor,
+    EntityExtractor,
+)
+from llama_index.ingestion import IngestionPipeline
 import toml
 import os
 import requests
@@ -46,34 +52,21 @@ class SourceType(str, Union[Path, str]):
             return cls.URL
         else:
             raise ValueError(f"Invalid source type for value: {value}")
-from llama_index import SimpleDirectoryReader
+
 
 class Library(BaseModel):
     documents: List[Document]
 
     def add(self, source: Union[Path, str]) -> None:
         # Load configuration
-        config = toml.load("config.toml")
+        config = toml.load("../config.toml")
         data_dir = config["paths"]["DATA"]
         storage_dir = config["paths"]["PERSIST"]
         os.makedirs(data_dir, exist_ok=True)
         os.makedirs(storage_dir, exist_ok=True)
 
         try:
-            # Determine if the source is a URL or a local file
-            source_type = SourceType.get_source_type(str(source))
-            if source_type == SourceType.URL:
-                # Source is a URL, download the file and save it locally
-                local_file_name = os.path.join(data_dir, os.path.basename(urlparse(source).path))
-                with requests.get(source, stream=True) as r:
-                    r.raise_for_status()
-                    with open(local_file_name, 'wb') as f:
-                        for chunk in r.iter_content(chunk_size=8192):
-                            f.write(chunk)
-                file_name = local_file_name
-            else:
-            elif source_type == SourceType.LOCAL_FILE:
-                file_name = str(source)  # Use the local file path directly
+            file_name = self._get_file(source, data_dir)
 
             # Read the document using SimpleDirectoryReader
             reader = SimpleDirectoryReader(input_files=[file_name])
@@ -82,12 +75,30 @@ class Library(BaseModel):
         except (ValueError, Exception) as e:
             print(f"Error handling the document source: {e}")
 
+    def _get_file(self, source: Union[Path, str], data_dir: str) -> str:
+        source_type = SourceType.get_source_type(str(source))
+        if source_type == SourceType.URL:
+            # Source is a URL, download the file and save it locally
+            local_file_name = os.path.join(data_dir, os.path.basename(urlparse(source).path))
+            with requests.get(source, stream=True) as r:
+                r.raise_for_status()
+                with open(local_file_name, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=8192):
+                        f.write(chunk)
+            return local_file_name
+        else:
+            # source_type == SourceType.LOCAL_FILE
+            # Source is a local file, use it directly
+            return str(source)
+
     def _extract_nodes(self, documents: List[Document]) -> List:
         pipeline = IngestionPipeline(
             transformations=[
-                SentenceSplitter(chunk_size=25, chunk_overlap=0),
-                TitleExtractor(),
-                OpenAIEmbedding(),
+                SentenceSplitter(chunk_size=256, chunk_overlap=64), #SentenceSplitter(chunk_size=512, chunk_overlap=128),
+                # TitleExtractor(nodes=5),
+                # QuestionsAnsweredExtractor(questions=3),
+                SummaryExtractor(summaries=["self"]),
+                KeywordExtractor(keywords=10),
             ]
         )
         nodes = pipeline.run(documents=documents)
